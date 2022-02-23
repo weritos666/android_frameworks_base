e5cbe691d6b59ff1123101a748a7f8c527234fb8
Christian Göllner <chrisgollner@google.com>
2022-02-23 15:23:33 +0100

    Motion: tweak split shade expansion on lockscreen with media.
    Changes the transformation from fade to transition.
    
    Bug: 200806458
    Bug: 218833796
    Bug: 218833458
    Test: Manually + Unit tests
    Change-Id: I762c9888bcb8fc62358b6c4ab8a70a4ece464e43
    Signed-off-by: strwbrry143 <alexfinhart@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
@@ -524,7 +524,8 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
         mContainer.setExpansion(expansion);
         final float translationScaleY = (mInSplitShade
                 ? 1 : QSAnimator.SHORT_PARALLAX_AMOUNT) * (expansion - 1);
-        boolean onKeyguardAndExpanded = isKeyguardState() && !mShowCollapsedOnKeyguard;
+        boolean onKeyguard = isKeyguardState();
+        boolean onKeyguardAndExpanded = onKeyguard && !mShowCollapsedOnKeyguard;
         if (!mHeaderAnimating && !headerWillBeAnimating()) {
             getView().setTranslationY(
                     onKeyguardAndExpanded
@@ -558,6 +559,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
                 mHeader.updateResources();
             }
         }
+        mQSPanelController.setIsOnKeyguard(onKeyguard);
         mFooter.setExpansion(onKeyguardAndExpanded ? 1 : expansion);
         mQSPanelController.setRevealExpansion(expansion);
         mQSPanelController.getTileLayout().setExpansion(expansion, proposedTranslation);
