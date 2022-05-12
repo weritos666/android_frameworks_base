406f3c127b1d07d268e1fccc109c68261a63dd65
Vania Desmonda <vaniadesmonda@google.com>
2022-05-12 13:52:09 +0100

    Prevent duplicate calls to updateSurface() and recursive call to updateSurfaceDimming().
    Reduce jank by removing notifyColorsChanged() on updateSurface() which
    caused notifyColorsChanged() to be called multiple times successively.
    
    Test: manual, atest WallpaperManagerTest
    Bug: 229715857
    Change-Id: I552a6956648250cd36787b7ddd79758422ea7f68

--- core/java/android/service/wallpaper/WallpaperService.java
+++ core/java/android/service/wallpaper/WallpaperService.java
@@ -867,7 +867,6 @@ public abstract class WallpaperService extends Service {
             if (mShouldDimByDefault != mShouldDim && mWallpaperDimAmount == 0f) {
                 mShouldDim = mShouldDimByDefault;
                 updateSurfaceDimming();
-                updateSurface(false, false, true);
             }
         }
 
@@ -877,13 +876,16 @@ public abstract class WallpaperService extends Service {
          * @param dimAmount Float amount between [0.0, 1.0] to dim the wallpaper.
          */
         private void updateWallpaperDimming(float dimAmount) {
+            if (dimAmount == mWallpaperDimAmount) {
+                return;
+            }
+
             // Custom dim amount cannot be less than the default dim amount.
             mWallpaperDimAmount = Math.max(mDefaultDimAmount, dimAmount);
             // If dim amount is 0f (additional dimming is removed), then the wallpaper should dim
             // based on its default wallpaper color hints.
             mShouldDim = dimAmount != 0f || mShouldDimByDefault;
             updateSurfaceDimming();
-            updateSurface(false, false, true);
         }
 
         private void updateSurfaceDimming() {
@@ -920,6 +922,7 @@ public abstract class WallpaperService extends Service {
             } else {
                 Log.v(TAG, "Setting wallpaper dimming: " + 0);
                 surfaceControlTransaction.setAlpha(mBbqSurfaceControl, 1.0f).apply();
+                updateSurface(false, false, true);
             }
 
             mPreviousWallpaperDimAmount = mWallpaperDimAmount;
@@ -1143,7 +1146,6 @@ public abstract class WallpaperService extends Service {
                                     .setParent(mSurfaceControl)
                                     .setCallsite("Wallpaper#relayout")
                                     .build();
-                            updateSurfaceDimming();
                         }
                         // Propagate transform hint from WM so we can use the right hint for the
                         // first frame.
@@ -1314,7 +1316,6 @@ public abstract class WallpaperService extends Service {
                             resetWindowPages();
                             mSession.finishDrawing(mWindow, null /* postDrawTransaction */);
                             processLocalColors(mPendingXOffset, mPendingXOffsetStep);
-                            notifyColorsChanged();
                         }
                         reposition();
                         reportEngineShown(shouldWaitForEngineShown());
