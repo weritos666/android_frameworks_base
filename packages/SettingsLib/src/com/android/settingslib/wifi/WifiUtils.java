bdd6037d35c18c30853719e7baf9d71dd7379f9f
Weng Su <wengsu@google.com>
2022-05-17 17:46:33 +0800

    Fix Wi-Fi level out of range crash
    - If the Wi-Fi level is less than the minimum level, use the minimum
      level instead.
    
    - If the Wi-Fi level is greater than the maximum level, use the
      maximum level instead.
    
    Bug: 230457323
    Test: manual test
    make RunSettingsLibRoboTests ROBOTEST_FILTER=WifiUtilsTest
    
    Change-Id: I7ec748ac200a7930ea08604ecf9cece6fcbfbb86

--- packages/SettingsLib/src/com/android/settingslib/wifi/WifiUtils.java
+++ packages/SettingsLib/src/com/android/settingslib/wifi/WifiUtils.java
@@ -28,6 +28,7 @@ import android.net.wifi.WifiConfiguration.NetworkSelectionStatus;
 import android.net.wifi.WifiInfo;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 
 import androidx.annotation.VisibleForTesting;
 
@@ -36,6 +37,8 @@ import com.android.settingslib.R;
 import java.util.Map;
 
 public class WifiUtils {
+
+    private static final String TAG = "WifiUtils";
 
     private static final int INVALID_RSSI = -127;
 
@@ -286,13 +289,17 @@ public class WifiUtils {
      *
      * @param level The number of bars to show (0-4)
      * @param noInternet True if a connected Wi-Fi network cannot access the Internet
-     * @throws IllegalArgumentException if an invalid RSSI level is given.
      */
     public static int getInternetIconResource(int level, boolean noInternet) {
-        if (level < 0 || level >= WIFI_PIE.length) {
-            throw new IllegalArgumentException("No Wifi icon found for level: " + level);
+        int wifiLevel = level;
+        if (wifiLevel < 0) {
+            Log.e(TAG, "Wi-Fi level is out of range! level:" + level);
+            wifiLevel = 0;
+        } else if (level >= WIFI_PIE.length) {
+            Log.e(TAG, "Wi-Fi level is out of range! level:" + level);
+            wifiLevel = WIFI_PIE.length - 1;
         }
-        return noInternet ? NO_INTERNET_WIFI_PIE[level] : WIFI_PIE[level];
+        return noInternet ? NO_INTERNET_WIFI_PIE[wifiLevel] : WIFI_PIE[wifiLevel];
     }
 
     /**
