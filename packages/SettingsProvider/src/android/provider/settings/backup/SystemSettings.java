8d4035a48b54f03da78a26f879f46110e6d9ef65
jhonboy121 <alfredmathew05@gmail.com>
2022-06-08 18:47:54 +0800

    base: Make notification artwork background toggleable [1/2]
    Why not?
    
    Signed-off-by: minarypenguin <alexfinhart@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -84,6 +84,7 @@ public class SystemSettings {
         Settings.System.DISPLAY_COLOR_MODE,
         Settings.System.ALARM_ALERT,
         Settings.System.NOTIFICATION_LIGHT_PULSE,
+        Settings.System.ARTWORK_MEDIA_BACKGROUND,
         Settings.System.ARTWORK_MEDIA_BACKGROUND_ALPHA,
         Settings.System.STATUS_BAR_LOGO,
         Settings.System.STATUS_BAR_LOGO_POSITION,
