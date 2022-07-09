92925f8d57a278a054e3701c9b14bb22be05c54d
minaripenguin <alexfinhart@gmail.com>
2022-07-09 22:08:26 +0800

    Revert "base: Add Accelerometer and Linear Acceleration sensors in blocking list"
    This reverts commit 7175435db55b8889c75a55865fdf03f888ab41cd.

--- core/java/android/hardware/SystemSensorManager.java
+++ core/java/android/hardware/SystemSensorManager.java
@@ -192,16 +192,13 @@ public class SystemSensorManager extends SensorManager {
                 + "the sensor listeners size has exceeded the maximum limit "
                 + MAX_LISTENER_COUNT);
         }
-            int sensortype = sensor.getType();
-            if (sensortype == Sensor.TYPE_SIGNIFICANT_MOTION ||
-                    sensortype == Sensor.TYPE_ACCELEROMETER ||
-                    sensortype == Sensor.TYPE_LINEAR_ACCELERATION) {
+        if (sensor.getType() == Sensor.TYPE_SIGNIFICANT_MOTION) {
             String pkgName = mContext.getPackageName();
             for (String blockedPkgName : mContext.getResources().getStringArray(
                     com.android.internal.R.array.config_blockPackagesSensorDrain)) {
                 if (pkgName.equals(blockedPkgName)) {
                     Log.w(TAG, "Preventing " + pkgName + "from draining battery using " +
-                                    sensor.getStringType());
+                                    "significant motion sensor");
                     return false;
                 }
             }
