46d760f26f542261f074383e60818edf99f6c54d
hydrangea07 <104114625+hydrangea07@users.noreply.github.com>
2022-11-02 06:56:41 +0800

    Revert "Add an option to force pre-O apps to use full screen aspect ratio"
    This reverts commit 8fa5c97347dea99acffa38911396bd0ef7aa5283.

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -8602,9 +8602,7 @@ public final class ActivityRecord extends WindowToken implements WindowManagerSe
         int activityHeight = containingAppHeight;
 
         if (containingRatio > desiredAspectRatio) {
-            if (mAtmService.shouldForceLongScreen(packageName)) {
-                // Use containingAppWidth/Height for maxActivityWidth/Height when force long screen
-            } else if (containingAppWidth < containingAppHeight) {
+            if (containingAppWidth < containingAppHeight) {
                 // Width is the shorter side, so we use that to figure-out what the max. height
                 // should be given the aspect ratio.
                 activityHeight = (int) ((activityWidth * desiredAspectRatio) + 0.5f);
